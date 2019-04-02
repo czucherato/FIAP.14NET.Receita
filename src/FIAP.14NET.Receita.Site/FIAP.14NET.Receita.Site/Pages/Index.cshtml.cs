8650f9b0a72ffb1822ef1dab414d9a6ad676f644
Carlos Alberto Zucherato <rm332329@fiap.com.br>
2019-04-02 20:53:47 -0300

    Adicionando o projeto do site

---
+++ src/FIAP.14NET.Receita.Site/FIAP.14NET.Receita.Site/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FIAP._14NET.Receita.Site.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
