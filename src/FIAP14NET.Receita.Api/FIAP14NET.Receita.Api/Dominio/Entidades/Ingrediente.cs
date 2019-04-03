4b63282f6e516339f547e0b1b2cf3a31c17f5968
Carlos Alberto Zucherato <rm332329@fiap.com.br>
2019-04-02 21:39:50 -0300

    Atualizando referências da api

---
+++ src/FIAP14NET.Receita.Api/FIAP14NET.Receita.Api/Dominio/Entidades/Ingrediente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FIAP14NET.Receita.Api.Dominio.ObjetosDeValor;
+
+namespace FIAP14NET.Receita.Api.Dominio.Entidades
+{
+    public class Ingrediente
+    {
+        public Ingrediente() => this.Id = new Guid();
+
+        public Ingrediente(string nome, Unidade unidade)
+            : this()
+        {
+            this.Nome = nome;
+            this.Unidade = unidade;
+        }
+
+        public Guid Id { get; protected internal set; }
+
+        public string Nome { get; protected internal set; }
+
+        public Unidade Unidade { get; protected internal set; }
+    }
+}
---
+++ src/FIAP14NET.Receita.Api/FIAP14NET.Receita.Api/Dominio/ObjetosDeValor/Unidade.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIAP14NET.Receita.Api.Dominio.ObjetosDeValor
+{
+    public enum Unidade
+    {
+        [Display(Name = "g")]
+        Gramas,
+        [Display(Name = "ml")]
+        Mililitros
+    }
+}
