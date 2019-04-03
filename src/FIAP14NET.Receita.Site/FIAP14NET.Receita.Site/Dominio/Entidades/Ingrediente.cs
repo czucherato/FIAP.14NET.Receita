54ea21221485ba36dd1d5332ffdbeec3a23aa1fd
Carlos Alberto Zucherato <rm332329@fiap.com.br>
2019-04-02 21:32:16 -0300

    Alterando referencias do projeto do site

---
+++ src/FIAP14NET.Receita.Site/FIAP14NET.Receita.Site/Dominio/Entidades/Ingrediente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FIAP14NET.Receita.Site.Dominio.ObjetosDeValor;
+
+namespace FIAP14NET.Receita.Site.Dominio.Entidades
+{
+    public class Ingrediente
+    {
+        public Ingrediente() => this.Id = new Guid();
+
+        public Ingrediente(string nome, Unidade unidade)
+            : this()
+        {
+            this.Nome = nome;
+            this.Unidade = unidade;
+        }
+
+        public Guid Id { get; protected internal set; }
+
+        public string Nome { get; protected internal set; }
+
+        public Unidade Unidade { get; protected internal set; }
+    }
+}
---
+++ src/FIAP14NET.Receita.Site/FIAP14NET.Receita.Site/Dominio/ObjetosDeValor/Unidade.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIAP14NET.Receita.Site.Dominio.ObjetosDeValor
+{
+    public enum Unidade
+    {
+        [Display(Name = "g")]
+        Gramas,
+        [Display(Name = "ml")]
+        Mililitros
+    }
+}
