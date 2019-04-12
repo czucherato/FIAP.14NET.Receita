1c497601ba6674b39a503bed4b99b9821543fbc4
Carlos Alberto Zucherato <carlos.zucheratto@gmail.com>
2019-04-12 07:47:01 -0300

    Adicionando projetos na nova estrutura

---
+++ projects/src/FIAP14NET.Receita.Site/FIAP14NET.Receita.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FIAP14NET.Receita.Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
