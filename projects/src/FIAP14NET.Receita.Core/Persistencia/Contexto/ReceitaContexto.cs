f5ea8de6b457f135e12aaaffe7a9cfa065c56ff2
Ricardo Dias <cadu.hd@gmail.com>
2019-04-14 20:32:47 -0300

    arrumar erro de versão do core e classe de contexto

--- projects/src/FIAP14NET.Receita.Core/Persistencia/Contexto/ReceitaContexto.cs
+++ projects/src/FIAP14NET.Receita.Core/Persistencia/Contexto/ReceitaContexto.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Linq;
+﻿using FIAP14NET.Receita.Core.Persistencia.Contexto.Mapeamentos;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using Entidades = FIAP14NET.Receita.Core.Dominio.Entidades;
-using FIAP14NET.Receita.Core.Persistencia.Contexto.Mapeamentos;
 
 namespace FIAP14NET.Receita.Core.Persistencia.Contexto
 {
     public class ReceitaContexto : DbContext
     {
-        public DbSet<Entidades.Receita> Receita { get; set; }
+        public ReceitaContexto(DbContextOptions<ReceitaContexto> options)
+                : base(options)
+        {
+        }
 
-        public DbSet<Entidades.Ingrediente> Ingrediente { get; set; }
+        public DbSet<Entidades.Receita> Receita
+        {
+            get; set;
+        }
+
+        public DbSet<Entidades.Ingrediente> Ingrediente
+        {
+            get; set;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Receita;Trusted_Connection=True;");
 
